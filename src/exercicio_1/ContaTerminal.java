762addc71611bb753ad1cfb2e6abcb0fccc9b3c5
Thiago <priotothiago@gmail.com>
2026-02-02 19:46:24 -0300

    Conta de Banco (Resolvido)

--- src/exercicio_1/ContaTerminal.java
+++ src/exercicio_1/ContaTerminal.java
@@ -1,7 +1,30 @@
 package exercicio_1;
 
-public class ContaTerminal {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args) throws InterruptedException{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao banco!");
+        Thread.sleep(1000);
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor digite o numero da agencia (exemplo: 0001): ");
+        int numero = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Digite o nome da agencia: ");
+        String nomeAgencia = sc.nextLine();
+
+        System.out.println("Digite seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %04d e seu saldo %.2f já está disponível para saque",
+                nomeCliente, nomeAgencia, numero, saldo);
 
     }
 }
